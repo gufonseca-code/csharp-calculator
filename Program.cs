1babf0c3aa9c863e44ae3800acf8c5c3470fe61b
Gustavo Oliveira <gufonseca.oliveira@gmail.com>
2025-07-20 09:30:06 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Digite dois números inteiros");
+string entrada1 = Console.ReadLine() ?? "";
+string entrada2 = Console.ReadLine() ?? "";
+
+if (int.TryParse(entrada1, out int primeiroNumero) && int.TryParse(entrada2, out int segundoNumero))
+{
+    Console.WriteLine($"Números digitados: {primeiroNumero} e {segundoNumero}\n");
+
+    int somaDosNumeros = primeiroNumero + segundoNumero;
+    int subtracaoDosNumeros = primeiroNumero - segundoNumero;
+    int multiplicacaoDosNumeros = primeiroNumero * segundoNumero;
+    double divisaoDosNumeros;
+    if (primeiroNumero != 0 && segundoNumero != 0)
+    {
+        divisaoDosNumeros = primeiroNumero / segundoNumero;
+    }
+    else
+    {
+        divisaoDosNumeros = 0;
+    }
+
+    Console.WriteLine($"Soma dos dois números: {somaDosNumeros}");
+    Console.WriteLine($"Subtracao dos dois números: {subtracaoDosNumeros}");
+    Console.WriteLine($"Multiplicação dos dois números: {multiplicacaoDosNumeros}");
+    Console.WriteLine($"Divisão dos dois números: {divisaoDosNumeros}");
+}
+else
+{
+    Console.WriteLine("Números inválidos");
+}   
+
